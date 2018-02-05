19d7750484a2dd1fa6769f65909f5aa9023e39c0
Pavel Novikov <pavel.b.novikov@gmail.com>
2018-02-06 04:47:04 +0700

    state info

--- Reinforced.VdomSample/Defs.ts
+++ Reinforced.VdomSample/Defs.ts
@@ -107,30 +107,6 @@
     /**
      * HTML parser state machine's state
      */
-    export interface IStateInfo {
-        /**
-         * Action to be performed single time when entering state
-         * @param ctx Context
-         * @param prevState Previous state
-         * @returns {} 
-         */
-        enter: (ctx: IParseContext, prevState: State) => void;
-
-        /**
-         * Action to be performed for each symbol coming from input HTML stream
-         * @param ctx Context
-         * @returns {} 
-         */
-        do: (ctx: IParseContext) => State;
-
-        /**
-         * Action to be performed single time when exiting state
-         * @param ctx Context
-         * @param nextState Next state
-         * @returns {} 
-         */
-        exit: (ctx: IParseContext, nextState: State) => void;
-    }
 
     //#endregion
 
---
+++ Reinforced.VdomSample/IStateInfo.ts
@@ -0,0 +1,26 @@
+﻿module Reinforced.Lattice.Html {
+    export interface IStateInfo {
+        /**
+         * Action to be performed single time when entering state
+         * @param ctx Context
+         * @param prevState Previous state
+         * @returns {} 
+         */
+        enter: (ctx: IParseContext, prevState: State) => void;
+
+        /**
+         * Action to be performed for each symbol coming from input HTML stream
+         * @param ctx Context
+         * @returns {} 
+         */
+        do: (ctx: IParseContext) => State;
+
+        /**
+         * Action to be performed single time when exiting state
+         * @param ctx Context
+         * @param nextState Next state
+         * @returns {} 
+         */
+        exit: (ctx: IParseContext, nextState: State) => void;
+    }
+}
